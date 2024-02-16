c7625ddf36ad64f5373a1b5f9c561c180e364e12
pon <lukasevicevgenij47@gmail.com>
2024-02-16 19:42:38 +0300

    kj

--- main/js.js
+++ main/js.js
@@ -1,5 +1,44 @@
+let choosed=1;
+
+update();
+
 function choose(x)
 {
     document.getElementsByClassName("nepon")[0].attributes.class.value="pon";
     x.attributes.class.value="nepon";
+    choosed=x.attributes.x.value;
+    switch (choosed)
+    {
+        case '1':
+            document.getElementById(`container1`).classList.remove("op0");
+            document.getElementById(`container2`).classList.remove("op1");
+            document.getElementById(`container3`).classList.remove("op1");
+            document.getElementById(`container3`).classList.add("op0");
+            document.getElementById(`container2`).classList.add("op0");
+            document.getElementById(`container1`).classList.add("op1");
+        break;
+        case '2':
+            document.getElementById(`container2`).classList.remove("op0");
+            document.getElementById(`container1`).classList.remove("op1");
+            document.getElementById(`container3`).classList.remove("op1");
+            document.getElementById(`container1`).classList.add("op0");
+            document.getElementById(`container3`).classList.add("op0");
+            document.getElementById(`container2`).classList.add("op1");
+        break;
+        case '3':
+            document.getElementById(`container3`).classList.remove("op0");
+            document.getElementById(`container1`).classList.remove("op1");
+            document.getElementById(`container2`).classList.remove("op1");
+            document.getElementById(`container1`).classList.add("op0");
+            document.getElementById(`container2`).classList.add("op0");
+            document.getElementById(`container3`).classList.add("op1");
+        break;
+    }
+    
+    update();
+}
+
+function update()
+{
+    
 }
